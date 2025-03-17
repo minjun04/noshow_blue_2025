407b34a454bd71140ebfbd16dec8f02dc2250feb
minjun04 <rhalswns0303@naver.com>
2025-03-17 17:14:10 +0900

    first commit

---
+++ src/main/java/noshow/Noshow_blue_2025/NoshowBlue2025Application.java
@@ -0,0 +1,13 @@
+package noshow.Noshow_blue_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoshowBlue2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoshowBlue2025Application.class, args);
+	}
+
+}
