e2b2c8b13cba4766d181a0d7dd3b9c1b01578b74
승현 <tmdgusdl0245@naver.com>
2025-03-31 16:08:56 +0900

    0331

--- src/main/java/noshow/Noshow_blue_2025/api/controller/dto/AuthLoginResponse.java
+++ src/main/java/noshow/Noshow_blue_2025/api/controller/dto/AuthLoginResponse.java
@@ -7,11 +7,13 @@ import noshow.Noshow_blue_2025.infra.entity.Student;
 @Data
 @Builder
 public class AuthLoginResponse {
+    private String studentId;
     private String name;
     private String email;
 
     public static AuthLoginResponse from(Student student) {
         return AuthLoginResponse.builder()
+                .studentId(student.getStudentId())
                 .name(student.getName())
                 .email(student.getEmail())
                 .build();
--- src/main/java/noshow/Noshow_blue_2025/domain/repositoryInterface/StudentRepository.java
+++ src/main/java/noshow/Noshow_blue_2025/domain/repositoryInterface/StudentRepository.java
@@ -4,5 +4,5 @@ import noshow.Noshow_blue_2025.infra.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student findByEmail(String Email);
+    Student findByEmail(String email);
 }
