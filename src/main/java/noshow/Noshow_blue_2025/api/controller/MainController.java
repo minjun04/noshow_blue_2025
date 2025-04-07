b1c10ff5672f64c938e413f18a6c0c8f1087a5b8
승현 <tmdgusdl0245@naver.com>
2025-04-07 15:31:20 +0900

    modify OAuth2

--- src/main/java/noshow/Noshow_blue_2025/api/controller/MainController.java
+++ src/main/java/noshow/Noshow_blue_2025/api/controller/MainController.java
@@ -1,2 +1,16 @@
-package noshow.Noshow_blue_2025.api.controller;public class MainController {
+package noshow.Noshow_blue_2025.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String mainAPI() {
+        System.out.println("야호");
+        return "main route";
+    }
 }
--- src/main/java/noshow/Noshow_blue_2025/api/controller/dto/AuthLoginRequest.java
+++ src/main/java/noshow/Noshow_blue_2025/api/controller/dto/AuthLoginRequest.java
@@ -5,5 +5,4 @@ import lombok.Data;
 @Data
 public class AuthLoginRequest {
     private String email;
-    private String password;
 }
